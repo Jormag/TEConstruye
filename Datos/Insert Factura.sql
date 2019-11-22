a38385aa38508605de0ff22d87c08d4ce118f0da
Yendry Diaz Solis <yenmari11@gmail.com>
2019-11-21 20:28:36 -0600

    Update Insert Factura.sql

--- Datos/Insert Factura.sql
+++ Datos/Insert Factura.sql
@@ -1,17 +1,17 @@
 USE [TEConstruye]
 GO
 
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (1,1 ,'2019-08-11',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (1,2 ,'2019-09-11',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (1,4 ,'2019-11-11',0)
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ) VALUES (1,1 ,'2019-08-11')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ) VALUES (1,2 ,'2019-09-11')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ) VALUES (1,4 ,'2019-11-11')
 
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (2,1 ,'2019-04-11',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (2,3 ,'2019-06-18',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (2,4 ,'2019-11-20',0)
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (2,1 ,'2019-04-11')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (2,3 ,'2019-06-18')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (2,4 ,'2019-11-20')
 
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (3,2 ,'2019-06-17',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (3,4 ,'2019-09-16',0)
-INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha] ,[Estado]) VALUES (3,8 ,'2019-11-11',0)
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (3,2 ,'2019-06-17')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (3,4 ,'2019-09-16')
+INSERT INTO [dbo].[Factura] ([IDObra] ,[IDEtapa] ,[fecha]) VALUES (3,8 ,'2019-11-11')
 GO
 
 
