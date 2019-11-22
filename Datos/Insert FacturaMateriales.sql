3a59f80d48ad8dbeeff51669fa1b55087e8d9837
Yendry Diaz Solis <yenmari11@gmail.com>
2019-11-21 22:17:48 -0600

    Create Insert FacturaMateriales.sql

---
+++ Datos/Insert FacturaMateriales.sql
@@ -0,0 +1,17 @@
+USE [TEConstruye]
+GO
+
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (1,10,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (2,15,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (3,17,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (4,18,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (5,35,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (6,176,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (7,140,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (8,17,0)
+INSERT INTO [dbo].[FacturaMateriales] ([IDFactura] ,[IDMaterial] ,[Neto]) VALUES (9,150,0)
+
+GO
+
+
+Select * from FacturaMateriales
