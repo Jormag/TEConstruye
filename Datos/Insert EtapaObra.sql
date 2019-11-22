1edb34cd9d8886969c5909cf8739b28a479ca12a
Cristian Marin <cristian-44@hotmail.es>
2019-11-22 00:00:09 -0600

    Update Insert EtapaObra.sql

--- Datos/Insert EtapaObra.sql
+++ Datos/Insert EtapaObra.sql
@@ -2,25 +2,25 @@ USE [TEConstruye]
 GO
 
 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (1,1,'11/08/2019' ,'10/09/2019' ,500000,480000 ,NULL)
+     VALUES (1,1,'2019-08-11' ,'2019-09-10' ,500000,480000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (1,2,'11/09/2019' ,'10/11/2019' ,600000,610000 ,NULL)
+     VALUES (1,2,'2019-09-11' ,'2019-11-10' ,600000,610000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (1,4,'11/11/2019' ,'11/12/2019' ,700000,710000 ,NULL)
+     VALUES (1,4,'2019-11-11' ,'2019-12-11' ,700000,710000 ,NULL)
 
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (2,1,'11/04/2019' ,'17/06/2019' ,550000,500000 ,NULL)
+     VALUES (2,1,'2019-04-11' ,'2019-06-17' ,550000,500000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (2,3,'18/06/2019' ,'19/11/2019' ,670000,700000 ,NULL)
+     VALUES (2,3,'2019-06-18' ,'2019-11-19' ,670000,700000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (2,5,'20/11/2019' ,'20/01/2020' ,820000,800000 ,NULL)
+     VALUES (2,5,'2019-11-20' ,'2020-01-20' ,820000,800000 ,NULL)
 
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (3,2,'17/06/2019' ,'15/09/2019' ,700000,710000 ,NULL)
+     VALUES (3,2,'2019-06-17' ,'2019-09-15' ,700000,710000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (3,4,'16/09/2019' ,'10/11/2019' ,800000,750000 ,NULL)
+     VALUES (3,4,'2019-09-16' ,'2019-11-10' ,800000,750000 ,NULL)
 	 INSERT INTO [dbo].[EtapaObra] ([IDObra],[IDEtapa] ,[FechaInicio],[FechaFin],[Presupuesto] ,[GastoEtapa],[Descripcion])
-     VALUES (3,8,'11/11/2019' ,'21/11/2019' ,1300000,1200000 ,NULL)
+     VALUES (3,8,'2019-11-11' ,'2019-11-21' ,1300000,1200000 ,NULL)
 GO
 
 
