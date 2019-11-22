80694bb8059fce32697407c15c1863c26489cb60
Yendry Diaz Solis <yenmari11@gmail.com>
2019-11-21 20:43:43 -0600

    Update Insert EmpleadoObra.sql

--- Datos/Insert EmpleadoObra.sql
+++ Datos/Insert EmpleadoObra.sql
@@ -1,26 +1,26 @@
 USE [TEConstruye]
 GO
 
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,8 ,'20-08-2019',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,9 ,'21-08-2019',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,6 ,'22-08-2019',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,9 ,'20-08-2019',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,8 ,'21-08-2019',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,5 ,'22-08-2019',NULL)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,8 ,'20-08-2019',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,9 ,'21-08-2019',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(203698231, 1 ,6 ,'22-08-2019',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,9 ,'20-08-2019',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,8 ,'21-08-2019',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(235641256, 1 ,5 ,'22-08-2019',0)
 
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,8 ,'2019-04-11',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,7 ,'2019-04-12',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,6 ,'2019-04-13',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,8 ,'2019-11-20',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,8 ,'2019-11-22',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,6 ,'2019-11-23',NULL)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,8 ,'2019-04-11',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,7 ,'2019-04-12',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(305830690, 2 ,6 ,'2019-04-13',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,8 ,'2019-11-20',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,8 ,'2019-11-22',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(321468036, 2 ,6 ,'2019-11-23',0)
 
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,8 ,'2019-09-16',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,6 ,'2019-09-17',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,8 ,'2019-09-19',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,8 ,'2019-11-11',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,7 ,'2019-11-12',NULL)
-INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,8 ,'2019-11-13',NULL)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,8 ,'2019-09-16',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,6 ,'2019-09-17',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(396203654, 3 ,8 ,'2019-09-19',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,8 ,'2019-11-11',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,7 ,'2019-11-12',0)
+INSERT INTO [dbo].[EmpleadoObra] ([IDEmpleado],[IDObra],[Horas] ,[Fecha] ,[PagoTrabajador]) VALUES(415286390, 3 ,8 ,'2019-11-13',0)
 
 
 GO
